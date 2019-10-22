5df63c5d466dc995bf1b55d2203d7e0f39a2c5e3
Brusenets Michael <k1brusenets@gmail.com>
2019-10-22 17:02:14 +0300

    crazy change for .hi.ua

--- script/scriptFB.js
+++ script/scriptFB.js
@@ -4,6 +4,7 @@ window.fbAsyncInit = function() {
         autoLogAppEvents: true,
         xfbml: true,
         status: true,
+        cookies: true,
         version: 'v4.0'
     });
 };
@@ -16,7 +17,33 @@ window.fbAsyncInit = function() {
     js.src = "https://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-FB.ui({
-    method: 'share',
-    href: 'https://developers.facebook.com/docs/',
-}, function(response) {});
+
+let sharing = document.querySelectorAll('.share');
+for (share of sharing) {
+    share.addEventListener('click', function(event) {
+        if (share == event.target) {
+            FB.ui({
+                    method: 'share_open_graph',
+                    action_type: 'og.shares',
+                    action_properties: JSON.stringify({
+                        object: {
+                            'og:url': 'http://firsttest.ho.ua/project1/testlogic1.html',
+                            'og:title': 'Я прошел сложнейший тест в Main Academy,',
+                            'og:description': '...и мне просто не терпится поделиться об этом невероятном событии со всеми моими друзьями!!!:',
+                            'og:image': 'http://firsttest.ho.ua/project1/images/back23.jpg'
+                        }
+                    })
+                },
+                // callback
+                function(response) {
+                    if (response && !response.error_message) {
+                        // then get post content
+                        alert('successfully posted. Status id : ' + response.post_id);
+                    } else {
+                        alert('Something went error.');
+                    }
+                })
+        }
+        break
+    })
+}
--- script/testlogic1.js
+++ script/testlogic1.js
@@ -1,3 +1,23 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '934445156889828',
+        autoLogAppEvents: true,
+        xfbml: true,
+        status: true,
+        cookies: true,
+        version: 'v4.0'
+    });
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 let a = 0,
     result = 0,
     visiblity = 0,
@@ -142,4 +162,39 @@ cityBlock.addEventListener('click', function(event) {
     templ = y
     templ = cityArea.innerHTML
         // console.log(templ)
+});
+
+
+
+
+let sharing = document.querySelectorAll('.share'),
+    body = document.getElementsByClassName('b1');
+
+body[0].addEventListener('click', function(event) {
+    for (share of sharing) {
+        if (share == event.target) {
+            FB.ui({
+                    method: 'share_open_graph',
+                    action_type: 'og.shares',
+                    action_properties: JSON.stringify({
+                        object: {
+                            'og:url': 'http://firsttest.ho.ua/project1/testlogic1.html',
+                            'og:title': 'Я прошел сложнейший тест в Main Academy,',
+                            'og:description': '...и мне просто не терпится поделиться об этом невероятном событии со всеми моими друзьями!!!:',
+                            'og:image': 'http://firsttest.ho.ua/project1/images/back23.jpg'
+                        }
+                    })
+                },
+                // callback
+                function(response) {
+                    if (response && !response.error_message) {
+                        // then get post content
+                        alert('successfully posted. Status id : ' + response.post_id);
+                    } else {
+                        alert('Something went error.');
+                    }
+                })
+        }
+    }
+
 });
